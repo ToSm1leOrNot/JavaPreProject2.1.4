43b45839785f3546244327e5a6bda9bc826bdd9d
ToSm1leOrNot <rakhmetilyas@gmail.com>
2022-12-17 17:41:14 +0300

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,27 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+    private Animal animal;
+    private Timer time;
+
+    public Timer getTimer() {
+        return time;
+    }
+    @Autowired
+    private AnimalsCage(@Qualifier ("dog") Animal animal, Timer time) {
+        this.animal = animal;
+        this.time = time;
+    }
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(time.getTime());
+        System.out.println("________________________");
+    }
+}
